abd179f466a8f11cb9e3e24494a16e32d16f7beb
nigeljanssens <njanssens@inlichtingenbureau.nl>
2024-07-15 16:38:13 +0200

    chore: add exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+/**
+ * A todo shape should look like this:
+ * {
+ *  id: number,
+ *   text: string,
+ *   completed: boolean
+ * }
+ */
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn HTML", completed: false },
+    { id: 2, text: "Learn CSS", completed: false },
+    { id: 3, text: "Learn Javascript", completed: false },
+    { id: 4, text: "Learn React", completed: false },
+    { id: 5, text: "Learn Nextjs", completed: false },
+  ]);
+
+  // TODO: Add logic to handle adding  todos
+  const handleAddTodo = () => {
+    console.log("adding todo");
+  };
+
+  // TODO: Add logic to handle deleting todos
+  const handleDeleteTodo = (id) => {
+    console.log("deleting todo", id);
+  };
+
+  // TODO: Add logic to handle completing todos
+  const handleCompleteTodo = (id) => {
+    console.log("completing todo", id);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      Todos
+      {/* List of todos */}
+      <ol className="todo-list">
+        {/* List of todo items here. The list should contain a list item for each todo, 
+        a button to delete the todo and a checkbox to mark the todo as completed */}
+      </ol>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
